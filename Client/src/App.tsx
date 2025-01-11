a06236604a527fb44cf20bb3d93e1c84a8030d9d
manish0kuniyal <manishkuniyal24@gmail.com>
2025-01-11 14:12:36 +0530

    test

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { io } from 'socket.io-client';
 
 function App() {
+  const [socket, setSocket] = useState(null);
+  const [isConnected, setIsConnected] = useState(false);
+  const [userId,setUserId]=useState(0)
+
+  useEffect(() => {
+    const newSocket = io('http://localhost:3000/');
+    setSocket(newSocket);
+
+    newSocket.on('connect', () => {
+      setIsConnected(true);
+      console.log('Connected to the server');
+      setUserId(newSocket.id)
+    });
+
+    newSocket.on('disconnect', () => {
+      setIsConnected(false);
+      console.log('Disconnected from the server');
+    });
+
+    return () => {
+      newSocket.disconnect();
+    };
+  }, []); 
+
   return (
-    <div className='border-2 text-green-400'>App</div>
-  )
+    <div className='text-center '>
+      <p>Connection - {userId} - {isConnected ? 'Connected' : 'Disconnected'} </p>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- Server/src/index.ts
+++ Server/src/index.ts
@@ -1,10 +1,38 @@
 import express, { Request, Response } from 'express';
 const app = express();
 
+app.use(express.json())
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World');
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+let url:{[key:string]:string}={}
+
+app.post('/url', (req: any, res: any) => {
+  const newUrl = req.body.newUrl;
+  if (!newUrl) {
+      return res.status(400).json({ error: 'URL is required' });
+  }
+  const mapUrl="https://randomurl.com/"+Math.random().toString(36).substring(7)
+  url[mapUrl]=newUrl
+
+  return res.json({ message: 'URL received', givenURL:newUrl ,mapUrl});
 });
+
+
+app.get('/:shortenUrl',(req:any,res:any)=>{
+  const shortenUrl=req.params.shortenUrl
+  const orignal=url[`https://randomurl.com/${shortenUrl}`]
+  if(orignal)
+    return res.redirect(orignal)
+
+  else
+  return res.status(404).json({error:"url not found"})
+})
+
+
+app.listen(3000, () => {
+  console.log('...on port 3000');
+});
+ 
